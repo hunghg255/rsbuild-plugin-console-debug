6594550ed9fcd0ed4d1ca8f22fe9bef338111f91
hunghg255 <giahunghust@gmail.com>
2023-12-05 08:20:02 +0700

    chore: update middleware

--- playground/rsbuild.config.ts
+++ playground/rsbuild.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
-import launchEditorMiddleware from 'launch-editor-middleware';
-// import { pluginConsoleDebug } from '../src/index';
 import { pluginConsoleDebug } from 'rsbuild-plugin-console-debug';
 
 export default defineConfig({
-  dev: {
-    setupMiddlewares: [
-      ({ unshift }, server) => {
-        unshift((req, res, next) => {
-          if (req.url.startsWith('/__open-in-editor')) {
-            launchEditorMiddleware()(req, res, next);
-            return;
-          }
-
-          next();
-        });
-      },
-    ],
-  },
   plugins: [pluginReact(), pluginConsoleDebug()],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import type { RsbuildPlugin } from '@rsbuild/core';
 
 import { resolvePackage } from '@rsbuild/shared';
+import { startServer } from './server/server';
+
+const PORT = 3070;
 
 export const pluginConsoleDebug = (): RsbuildPlugin => {
   return {
@@ -10,27 +13,9 @@ export const pluginConsoleDebug = (): RsbuildPlugin => {
         return;
       }
 
-      // FIX: not work when modify devServer
-      // api.modifyRsbuildConfig((config) => {
-      //   const setupMiddlewares = config.dev?.setupMiddlewares || [];
-
-      //   setConfig(config, 'dev.setupMiddlewares', [
-      //     ...setupMiddlewares,
-      //     ({ unshift }, server) => {
-      //       unshift((req, res, next) => {
-      //         if (req.url.startsWith('/__open-in-editor1')) {
-      //           launchEditorMiddleware()(req, res, next);
-      //           return;
-      //         }
-
-      //         next();
-      //       });
-      //     },
-      //   ]);
-      //   setConfig(config, 'dev.port', 3000)
-
-      //   return config;
-      // });
+      api.onAfterStartDevServer(({ port }) => {
+        startServer(PORT);
+      });
 
       api.modifyBundlerChain(async (chain, utils) => {
         chain.module
@@ -39,7 +24,7 @@ export const pluginConsoleDebug = (): RsbuildPlugin => {
           .use(utils.CHAIN_ID.RULE.TS)
           .loader(resolvePackage('./core/applyConcoleDebug.js', __dirname))
           .options({
-            port: api.context?.devServer?.port || 8080,
+            port: PORT,
           })
           .end();
       });
